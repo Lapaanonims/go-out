78f81bb00d848b0c7ee123a70a613a0a09f80bec
valaptev <valaptev@vp.ru>
2016-08-20 13:03:16 +0400

    started

---
+++ go-out/go-out/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(go_out.Startup))]
+namespace go_out
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
